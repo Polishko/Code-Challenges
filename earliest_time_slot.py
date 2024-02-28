88eea03196e477b27f8efbbaa2aba0e01c0a1825
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2024-02-28 11:58:26 +0200

    Update earliest_time_slot.py
    Added explanations

--- earliest_time_slot.py
+++ earliest_time_slot.py
@@ -1,5 +1,9 @@
+# For a given list of occupied slots in a company's working hours and a duration for a meeting, finds the first available time slot that is not occupied and allows for the meeting to be held for the given duration.
+# Occupied slots with starting and ending times are input in the form of tuples.
+
 from datetime import datetime, timedelta
-START = '8:00'
+# Company working hours
+START = '8:00' 
 END = '17:00'
 
 
@@ -7,6 +11,7 @@ def find_earliest_slot(occupied_main_list, duration_hours):
     all_slots = set()
     occupied_slots = set()
 
+    # Create a set with all the slots in a working day
     start = datetime.strptime(START, '%H:%M')
     end = datetime.strptime(END, '%H:%M')
     current = start
@@ -15,8 +20,10 @@ def find_earliest_slot(occupied_main_list, duration_hours):
         all_slots.add(current)
         current += timedelta(hours=1)
 
+    # Unpack the main list with the schedules to as single list
     occupied_sub_list = [item for sub_list in occupied_main_list for item in sub_list]
 
+    # Create a set with all occupied slots
     for slot in occupied_sub_list:
         start = datetime.strptime(slot[0], '%H:%M')
         end = datetime.strptime(slot[1], '%H:%M')
@@ -32,6 +39,7 @@ def find_earliest_slot(occupied_main_list, duration_hours):
 
     duration = timedelta(hours=duration_hours)
 
+    # Check for the first available slot
     for i in range(len(sorted_free_slots) - 1):
         current_slot = sorted_free_slots[i]
         next_slot = sorted_free_slots[i + 1]
